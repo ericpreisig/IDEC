64d47ff3c62a87d87d274b2260c1420d11a51dea
Eric Preisig <ericpreisig@gmail.com>
2023-01-18 19:11:29 +0100

    Add exercice 1 bis with a for loop

---
+++ Session 2/Corrections/Exercice1Bis/Program.cs
@@ -0,0 +1,15 @@
+﻿int resultat = int.MinValue;
+
+for (int i = 0; i < 3; i++)
+{
+    Console.Write("Veuillez entrer le nombre n° " + (i + 1) + ": ");
+    int nombre = int.Parse(Console.ReadLine());
+
+    if (nombre > resultat)
+    {
+        resultat = nombre;
+    }
+}
+
+Console.WriteLine("Le chiffre le plus grand est : " + resultat);
+Console.ReadLine();
