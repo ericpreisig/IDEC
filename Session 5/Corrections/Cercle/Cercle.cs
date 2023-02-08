783e60448e204eb98e18a3abf0cc3ed7ff31fa95
Eric Preisig <ericpreisig@gmail.com>
2023-02-08 18:54:21 +0100

    Add exercice Cercle

---
+++ Session 5/Corrections/Cercle/Cercle.cs
@@ -0,0 +1,21 @@
+﻿namespace Cercle
+{
+    public class Cercle
+    {
+        private readonly double _rayon;
+
+        public Cercle(double rayon)
+        {
+            _rayon = rayon;
+        }
+
+        public double GetSurface()
+        {
+            return Math.PI * _rayon * _rayon;
+        }
+        public double GetPerimetre()
+        {
+            return 2 * Math.PI * _rayon;
+        }
+    }
+}
---
+++ Session 5/Corrections/Cercle/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Cercle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cercle cercle1 = new Cercle(10);
+            Cercle cercle2 = new Cercle(20);
+
+            Console.WriteLine($"L'air du cercle 1 est de : {cercle1.GetSurface()}");
+            Console.WriteLine($"L'air du cercle 2 est de : {cercle2.GetSurface()}");
+
+            Console.WriteLine($"Le périmètre du cercle 1 est de : {cercle1.GetPerimetre()}");
+            Console.WriteLine($"Le périmètre du cercle 2 est de : {cercle2.GetPerimetre()}");
+        }
+    }
+}
