fa77001e131e80c5e888610fae6483c240a17d89
Eric Preisig <ericpreisig@gmail.com>
2024-01-30 18:10:20 +0100

    Ajout du cours 4.1

---
+++ Session 4.1/Corrections/Exercice1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercice1
+{
+    internal class Program
+    {
+        private static List<int> _seenList = new List<int>();
+        private static List<int> _notSeenList = Enumerable.Range(1, 100).ToList();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jeu du déjà-vu");
+            Random random = new Random();
+
+            bool endGame = false;
+            int score = 0;
+
+            while (!endGame && _notSeenList.Count > 0)
+            {
+                int displayNumber = ChooseNumber(random);
+                Console.WriteLine($"Avez-vous déjà vu ce nombre : {displayNumber} ? (oui/non)");
+                string input = GetUserInput();
+
+                if ((input == "oui" && !_seenList.Contains(displayNumber)) || (input == "non" && _seenList.Contains(displayNumber)))
+                {
+                    endGame = true;
+                }
+
+                UpdateLists(displayNumber, input);
+
+                if (!endGame) score++;
+                Console.Clear();
+            }
+
+            Console.WriteLine(_notSeenList.Count == 0 ? "Vous avez gagné !" : "Vous avez perdu !");
+            Console.WriteLine($"Votre score est de {score}");
+        }
+
+        static int ChooseNumber(Random random)
+        {
+            bool showNewNumber = _seenList.Count == 0 || random.Next(0, 2) == 0;
+            return showNewNumber ? _notSeenList[random.Next(_notSeenList.Count)] : _seenList[random.Next(_seenList.Count)];
+        }
+
+        static string GetUserInput()
+        {
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+            } while (input != "oui" && input != "non");
+
+            return input;
+        }
+
+        static void UpdateLists(int number, string input)
+        {
+            if (input == "non" && !_seenList.Contains(number))
+            {
+                _seenList.Add(number);
+                _notSeenList.Remove(number);
+            }
+        }
+    }
+}
---
+++ Session 4.1/Corrections/Exercice2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var deck = GenerateDeck();
+        var player1Deck = new List<int>(deck.Take(26));
+        var player2Deck = new List<int>(deck.Skip(26));
+
+        int roundCount = 0;
+
+        while (player1Deck.Any() && player2Deck.Any())
+        {
+            Console.WriteLine("Appuyez sur « Enter » pour jouer un tour...");
+            Console.ReadLine();
+
+            PlayRound(player1Deck, player2Deck);
+            roundCount++;
+        }
+
+        Console.WriteLine($"Nombre de tours joués : {roundCount}");
+        if (player1Deck.Any())
+        {
+            Console.WriteLine("Joueur 1 gagne le jeu !");
+        }
+        else
+        {
+            Console.WriteLine("Joueur 2 gagne le jeu !");
+        }
+    }
+
+    static void PlayRound(List<int> player1Deck, List<int> player2Deck)
+    {
+        if (!player1Deck.Any() || !player2Deck.Any())
+        {
+            return;
+        }
+
+        var player1Card = player1Deck.First();
+        player1Deck.RemoveAt(0);
+        var player2Card = player2Deck.First();
+        player2Deck.RemoveAt(0);
+
+        Console.WriteLine($"Joueur 1 joue : {CardToString(player1Card)}");
+        Console.WriteLine($"Joueur 2 joue : {CardToString(player2Card)}");
+
+        List<int> cardsOnTable = new List<int> { player1Card, player2Card };
+
+        if (player1Card > player2Card)
+        {
+            Console.WriteLine("Joueur 1 remporte la manche !");
+            player1Deck.AddRange(cardsOnTable);
+        }
+        else if (player2Card > player1Card)
+        {
+            Console.WriteLine("Joueur 2 remporte la manche !");
+            player2Deck.AddRange(cardsOnTable);
+        }
+        else
+        {
+            Console.WriteLine("Bataille !");
+            if (player1Deck.Count > 0 && player2Deck.Count > 0)
+            {
+                PlayRound(player1Deck, player2Deck);
+            }
+        }
+    }
+
+    static List<int> GenerateDeck()
+    {
+        var deck = new List<int>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 2; j <= 14; j++)
+            {
+                deck.Add(j);
+            }
+        }
+
+        var random = new Random();
+        deck = deck.OrderBy(x => random.Next()).ToList();
+        return deck;
+    }
+
+    static string CardToString(int cardValue)
+    {
+        switch (cardValue)
+        {
+            case 11:
+                return "Valet";
+            case 12:
+                return "Dame";
+            case 13:
+                return "Roi";
+            case 14:
+                return "As";
+            default:
+                return cardValue.ToString();
+        }
+    }
+}
